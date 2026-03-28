c500c31ea81c24c4b5503723a47d56a0942e601c
Eobam <elliottlieberman123@gmail.com>
2026-03-27 21:45:25 -0400

    reqs

---
+++ main.py
@@ -0,0 +1,42 @@
+# imports, will include all at top cuz i don't feel like organizing them by type as i normally do
+from flask import Flask, render_template, request, send_from_directory, jsonify
+import os
+
+app = Flask(__name__)
+
+#folders
+video_upload_folder = 'uploads'
+os.makedirs(video_upload_folder, exist_ok=True)
+
+a_pass = '1234567890'
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+
+    if request.method == 'POST':
+        video_file = request.files.get('video_file')
+        password = request.form.get('password')
+        
+
+        if password != a_pass:
+            return jsonify({'error': 'Incorrect Password'}), 403
+
+
+        if video_file:
+            video_file.save(os.path.join(video_upload_folder, video_file.filename))
+            return jsonify({'filename': video_file.filename})
+
+    return render_template('index.html')
+
+@app.route('/video/<filename>')
+def serve_video(filename):
+    return send_from_directory(video_upload_folder, filename)
+
+@app.route('/videos')
+def list_videos():
+    files = os.listdir(video_upload_folder)
+    files.sort(key=lambda f: os.path.getmtime(os.path.join(video_upload_folder, f)), reverse=True)
+    return jsonify(files)
+
+if __name__ == '__main__':
+    app.run(debug=True)
